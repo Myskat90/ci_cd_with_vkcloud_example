e49df7a4a7faebcc19053936485aecef71bfbdcc
Подмосковный Александр <podmoskovniy@ap.com>
2023-04-25 09:19:01 +0300

    setup django project v4.0.9 for k8s

--- infrastructure/14_ansible_k8s.sh
+++ infrastructure/14_ansible_k8s.sh
@@ -7,8 +7,11 @@ run_command "tree ansible-k8s"
 echo "Введите github token:"
 read -s github_token
 
+echo "Введите godaddy token:"
+read -s godaddy_token
+
 github_owner=$(git remote -v | head -n 1 | cut -d ":" -f2 | cut -d "/" -f4)
 github_repo=$(git remote -v | head -n 1 | cut -d ":" -f2 | cut -d "/" -f5| cut -d "." -f1)
 
-run_command "ansible-playbook main.yml -i 'localhost' -e github_owner=$github_owner -e github_repo=$github_repo -e github_authtoken=$github_token -v" "ansible-k8s"
+run_command "ansible-playbook main.yml -i 'localhost' -e github_owner=$github_owner -e github_repo=$github_repo -e github_authtoken=$github_token -e godaddy_authtoken=$godaddy_token -v" "ansible-k8s"
 
--- infrastructure/terraform-k8s/main.tf
+++ infrastructure/terraform-k8s/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     vkcs = {
-      source = "vk-cs/vkcs"
+      source  = "vk-cs/vkcs"
     }
   }
 }
